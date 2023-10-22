cf02520441d89f13269898e54652aca5febb8161
he ting <2975587905@qq.com>
2023-10-22 18:59:12 +0800

    Simple UI update

--- musicPlayer.asm
+++ musicPlayer.asm
@@ -31,6 +31,9 @@ includelib oleaut32.lib
 includelib msvcrt.lib
 includelib winmm.lib
 
+WinMain proto:DWORD, :DWORD, :DWORD, :DWORD
+InitInstance proto:HINSTANCE,:DWORD
+
 
 .data
 szWindowClass db "MusicPlayer",0
@@ -39,7 +42,7 @@ AppName db "音乐播放器v1.0", 0
 szTextFileName db "文件名",0
 szTextFileSize db "文件大小",0
 
-fontname db 'Consolas', 0
+fontname db 'Arial', 0
 text_button db "button", 0
 text_play db "开始播放", 0
 text_stop db "停止播放", 0
@@ -50,6 +53,7 @@ text_edit db "编辑",0
 text_opendir db "打开目录", 0
 text_browse_folder db "Browse folder", 0
 text_listbox db "listbox", 0
+text_showtext db "文件列表",0
 
 
 find_dir_fmt db '%', 0, 's', 0, '\', 0, '*',0, 0, 0
@@ -66,19 +70,21 @@ startplay_fmt db '%', 0, 's', 0, '\', 0, '%', 0, 's', 0, 0, 0
 ;"%s\\%s"
 stopplay_fmt db '00:00', 0
 ;"00:00"
+text_static db "static",0
 text_0000 db "00:00",0
+PROGRESSCLASS db "msctls_progress32",0
 
 ; 控件的全局ID
-IDC_PLAYBTN HMENU 205
-IDC_LISTVIEW HMENU 206
-IDC_OPENDIRBTN HMENU 207
-IDC_EDIT HMENU 208
-IDC_CURRENTPLAYTIME HMENU 209
-IDC_TOTALPLAYTIME HMENU 210
-IDC_STOPBTN HMENU 211
-IDC_PROGRESSBAR HMENU 212
-IDC_LIST HMENU 213
-IDC_REPEATE HMENU 214
+IDC_TEXT HMENU 206
+IDC_LISTBOX HMENU 207
+IDC_PROGRESSBAR HMENU 208
+IDC_CURRENTTIME HMENU 209
+IDC_TOTALTIME HMENU 210
+IDC_PLAYBTN HMENU 211
+IDC_FILESELECTBTN HMENU 212
+IDC_PAUSEBTN HMENU 213
+IDC_STOPBTN HMENU 214
+IDC_REPEATE HMENU 215
 
 .data?
 hInstance HINSTANCE ?
@@ -86,16 +92,17 @@ CommandLine LPSTR ?
 
 ; 控件的Handle
 hFont HFONT ?
+hText HWND ?
+hListBox HWND ?
+hProgressBar HWND ?
+hCurrentTime HWND ?
+hTotalTime HWND ?
 hPlayBtn HWND ?
-hwndPB HWND ?
-hListView HWND ?
-hOpenDirBtn HWND ?
-hwndEdit HWND ?
+hFileSelectBtn HWND ?
+hPauseBtn HWND ?
 hStopBtn HWND ?
-hTotalPlayTime HWND ?
-hCurrentPlayTime HWND ?
-hList HWND ?
-hRepeatBtn HWND ?
+hRepeate HWND ?
+
 
 
 .code
@@ -108,68 +115,286 @@ start:
 	invoke WinMain, hInstance, NULL, CommandLine, SW_SHOWDEFAULT
 	invoke ExitProcess, eax
     ;注册窗口通用语法
-	WinMain proc hInst:HINSTANCE
-        LOCAL wc:WNDCLASSEX              
-        LOCAL msg:MSG 
-        LOCAL hwnd:HWND 
+    MyRegisterClass proc hInst:HINSTANCE
+	    LOCAL wcex: WNDCLASSEXW
+	    mov wcex.cbSize, sizeof WNDCLASSEX
+	    mov wcex.style, CS_HREDRAW or CS_VREDRAW
+	    mov wcex.lpfnWndProc, OFFSET WndProc 
+	    mov wcex.cbClsExtra, NULL
+	    mov wcex.cbWndExtra, NULL
+	    push hInst
+	    pop wcex.hInstance
+	    mov wcex.hbrBackground, COLOR_WINDOW + 1
+	    mov wcex.lpszMenuName, NULL
+	    mov wcex.lpszClassName ,offset szWindowClass
+	    invoke LoadIcon, NULL, IDI_APPLICATION
+	    mov wcex.hIcon, eax
+	    mov wcex.hIconSm, eax
+	    invoke LoadCursor, NULL, IDC_ARROW
+	    mov wcex.hCursor, eax
+	    invoke RegisterClassEx, addr wcex
+	    ret
+    MyRegisterClass endp
 
-        mov   wc.cbSize,SIZEOF WNDCLASSEX   
-        mov   wc.style, CS_HREDRAW or CS_VREDRAW 
-        mov   wc.lpfnWndProc, OFFSET WndProc 
-        mov   wc.cbClsExtra,NULL 
-        mov   wc.cbWndExtra,NULL 
-        push  hInstance 
-        pop   wc.hInstance 
-        mov   wc.hbrBackground,COLOR_WINDOW+1 
-        mov   wc.lpszMenuName,NULL 
-        mov   wc.lpszClassName,OFFSET ClassName 
-        invoke LoadIcon,NULL,IDI_APPLICATION 
-        mov   wc.hIcon,eax 
-        mov   wc.hIconSm,eax 
-        invoke LoadCursor,NULL,IDC_ARROW 
-        mov   wc.hCursor,eax 
-        invoke RegisterClassEx, addr wc   
-        invoke CreateWindowEx,NULL,\ 
-                    ADDR ClassName,\ 
-                    ADDR AppName,\ 
-                    WS_OVERLAPPEDWINDOW,\ 
-                    0,\ 
-                    0,\ 
-                    300,\ 
-                    300,\ 
-                    NULL,\ 
-                    NULL,\ 
-                    hInst,\ 
-                    NULL 
-        mov   hwnd,eax 
-        invoke ShowWindow, hwnd,SW_SHOWDEFAULT   
-        invoke UpdateWindow, hwnd               
+    WinMain proc hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine: LPSTR, CmdShow:DWORD
+	    local msg :MSG
 
-        .WHILE TRUE                                             
-		    invoke GetMessage, ADDR msg,NULL,0,0 
-            .BREAK .IF (!eax) 
-            invoke TranslateMessage, ADDR msg 
-            invoke DispatchMessage, ADDR msg 
-       .ENDW 
-        mov     eax,msg.wParam                      
-        ret 
+
+	    invoke MyRegisterClass, hInst
+	    invoke InitInstance, hInst, CmdShow
+
+	    ; 主消息循环
+	    .while TRUE
+		    invoke GetMessage, addr msg, NULL, 0, 0
+		    .break .if (!eax)
+		    invoke TranslateMessage, addr msg
+		    invoke DispatchMessage, addr msg
+	    .endw
+
+	    mov eax, msg.wParam
+	    ret
+
     WinMain endp
+
+
+
+    InitInstance proc hInst: HINSTANCE, CmdShow :DWORD
+	    local hwnd:HWND
+	    invoke CreateWindowEx, NULL, addr szWindowClass, addr szTitle, 
+		    WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 600, 
+		    NULL, NULL, hInst, NULL
+	    mov hwnd, eax
+
+	    .if (!eax)
+		    ret
+	    .endif
+	
+	    invoke ShowWindow, hwnd, SW_SHOWNORMAL
+	    invoke UpdateWindow, hwnd
+
+	    ret
+    InitInstance endp 
     
 
     ;创建控件
     CreateWindowControl proc uses eax hWnd:HWND
+        ;创建字体
+	    invoke CreateFont, 12 , 0, 0, 0, FW_NORMAL,
+		    FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+		    OUT_CHARACTER_PRECIS,CLIP_CHARACTER_PRECIS,
+		    DEFAULT_QUALITY,FF_DONTCARE,
+		    offset fontname
+	    mov hFont, eax
+        ;创建固定文本框
+        invoke CreateWindowEx, 0, offset text_static, offset text_showtext,
+		    WS_CHILD or WS_VISIBLE or SS_LEFT,
+		    0, 0, 0, 0,
+		    hWnd, IDC_TEXT, hInstance, NULL
+	    mov hText, eax
+	    invoke SendMessage, hText, WM_SETFONT, hFont, NULL
+        
+        ;创建listbox
+        invoke CreateWindowEx, WS_EX_CLIENTEDGE, offset text_listbox, NULL,
+              WS_CHILD or WS_VISIBLE or LBS_STANDARD,
+              0, 0, 0, 0,
+              hWnd, IDC_LISTBOX, hInstance, NULL
+        mov hListBox, eax
+        invoke SendMessage, hListBox, WM_SETFONT, hFont, NULL
+
+        ;创建进度条
+        invoke CreateWindowEx, 0,offset PROGRESSCLASS, NULL,
+              WS_CHILD or WS_VISIBLE,
+              0, 0, 0, 0,
+              hWnd, IDC_PROGRESSBAR, hInstance, NULL
+        mov hProgressBar, eax
+        invoke SendMessage, hProgressBar, WM_SETFONT, hFont, NULL
+
+        ;创建当前时间文本框
+        invoke CreateWindowEx, 0, offset text_static, offset text_0000,
+              WS_CHILD or WS_VISIBLE or SS_RIGHT,
+              0, 0, 0, 0,
+              hWnd, IDC_CURRENTTIME, hInstance, NULL
+        mov hCurrentTime, eax
+        invoke SendMessage, hCurrentTime, WM_SETFONT, hFont, NULL
+
+        ;创建总时间文本框
+        invoke CreateWindowEx, 0, offset text_static, offset text_0000,
+              WS_CHILD or WS_VISIBLE or SS_LEFT,
+              0, 0, 0, 0,
+              hWnd, IDC_TOTALTIME, hInstance, NULL
+        mov hTotalTime, eax
+        invoke SendMessage, hTotalTime, WM_SETFONT, hFont, NULL
+
+        ;创建打开新目录按钮
+        invoke CreateWindowEx, 0, offset text_button, offset text_opendir,
+              WS_CHILD or WS_VISIBLE or BS_PUSHBUTTON,
+              0, 0, 0, 0,
+              hWnd, IDC_FILESELECTBTN, hInstance, NULL
+        mov hFileSelectBtn, eax
+        invoke SendMessage, hFileSelectBtn, WM_SETFONT, hFont, NULL
+
+        ;创建播放按钮
+        invoke CreateWindowEx, 0, offset text_button, offset text_play,
+              WS_CHILD or WS_VISIBLE or BS_PUSHBUTTON,
+              0, 0, 0, 0,
+              hWnd, IDC_PLAYBTN, hInstance, NULL
+        mov hPlayBtn, eax
+        invoke SendMessage, hPlayBtn, WM_SETFONT, hFont, NULL
+
+        ;创建暂停按钮
+        invoke CreateWindowEx, 0, offset text_button, offset text_pause,
+              WS_CHILD or WS_VISIBLE or BS_PUSHBUTTON,
+              0, 0, 0, 0,
+              hWnd, IDC_PAUSEBTN, hInstance, NULL
+        mov hPauseBtn, eax
+        invoke SendMessage, hPauseBtn, WM_SETFONT, hFont, NULL
+
+        ;创建停止按钮
+        invoke CreateWindowEx, 0, offset text_button, offset text_stop,
+              WS_CHILD or WS_VISIBLE or BS_PUSHBUTTON,
+              0, 0, 0, 0,
+              hWnd, IDC_STOPBTN, hInstance, NULL
+        mov hStopBtn, eax
+        invoke SendMessage, hStopBtn, WM_SETFONT, hFont, NULL
+
+        ;创建单曲循环复选框
+        invoke CreateWindowEx, 0, offset text_button, offset text_repeat,
+              WS_CHILD or WS_VISIBLE or BS_AUTOCHECKBOX,
+              0, 0, 0, 0,
+              hWnd, IDC_REPEATE, hInstance, NULL
+        mov hRepeate, eax
+        invoke SendMessage, hRepeate, WM_SETFONT, hFont, NULL
+
+        ret
     CreateWindowControl endp
 
 
     ; 调整控件位置
     ReSizeWindowControl proc uses eax ebx hWnd:HWND
+         local rc:RECT
+         local current_width:DWORD
+	     local current_height:DWORD
+         local playBtn_bottom:DWORD
+         local middle:DWORD
+         local temleft:DWORD
+         local temwidth:DWORD
+         local temheight:DWORD
+         local temtop:DWORD
+         ;获取窗口大小
+         invoke GetClientRect, hWnd, addr rc 
+         mov ebx, rc.right
+	     sub ebx, rc.left
+	     mov current_width, ebx
+         mov ebx, rc.bottom
+	     sub ebx, rc.top
+	     mov current_height, ebx
+         mov eax,current_width
+         mov ebx,2
+         mov edx,0
+         div ebx
+         mov middle,eax ;current_width/2
+         mov eax, 0 
+         ;调整文本框位置 
+         mov eax,rc.top
+         mov temtop,eax
+         add temtop,10
+         mov eax,rc.left
+         mov temleft,eax
+         add temleft,20
+         invoke MoveWindow, hText,temleft,temtop, 45,15, TRUE
+         ;调整列表框位置 
+         mov eax,rc.top
+         mov temtop,eax
+         add temtop,30
+         mov eax,rc.left
+         mov temleft,eax
+         add temleft,20
+         mov eax,current_width
+         mov temwidth,eax
+         sub temwidth,40
+         mov eax,current_height
+         mov temheight,eax
+         sub temheight,250
+         invoke MoveWindow, hListBox,temleft,temtop, temwidth,temheight, TRUE 
+         ;调整进度条位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,70
+         mov eax,rc.left
+         mov temleft,eax
+         add temleft,80
+         mov eax,current_width
+         mov temwidth,eax
+         sub temwidth,160
+         invoke MoveWindow, hProgressBar, temleft,temtop, temwidth, 15, TRUE 
+         ;调整当前时间文本框位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,70
+         mov eax,rc.left
+         mov temleft,eax
+         add temleft,20
+         invoke MoveWindow, hCurrentTime,temleft,temtop, 55, 15, TRUE  
+         ;调整总时间文本框位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,70
+         mov eax,rc.right
+         mov temleft,eax
+         sub temleft,75
+         invoke MoveWindow, hTotalTime,temleft,temtop, 55, 15, TRUE 
+         ;调整打开新目录按钮位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,40
+         mov eax,middle
+         mov temleft,eax
+         sub temleft,180
+         invoke MoveWindow, hFileSelectBtn,temleft,temtop, 60, 30, TRUE 
+         ;调整播放按钮位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,40
+         mov eax,middle
+         mov temleft,eax
+         sub temleft,100
+         invoke MoveWindow, hPlayBtn, temleft,temtop, 60, 30, TRUE 
+         ;调整暂停按钮位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,40
+         mov eax,middle
+         mov temleft,eax
+         sub temleft,20
+         invoke MoveWindow, hPauseBtn,temleft, temtop, 60, 30, TRUE 
+         ;调整停止按钮位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,40
+         mov eax,middle
+         mov temleft,eax
+         add temleft,60
+         invoke MoveWindow, hStopBtn, temleft,temtop, 60, 30, TRUE 
+         ;调整单曲循环复选框位置 
+         mov eax,rc.bottom
+         mov temtop,eax
+         sub temtop,40
+         mov eax,middle
+         mov temleft,eax
+         add temleft,140
+         invoke MoveWindow, hRepeate, temleft,temtop, 60, 30, TRUE
+         ret
     ReSizeWindowControl endp
 
 
     ;窗口操作
     WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM 
         .if uMsg == WM_DESTROY
-		    invoke PostQuitMessage,NULL 
+		    invoke PostQuitMessage,NULL
+        ;创建控件
+        .elseif uMsg == WM_CREATE
+		    invoke InitCommonControls
+		    invoke CreateWindowControl, hWnd
         ; 窗口变化大小
 	    .elseif uMsg == WM_SIZE
 		    invoke ReSizeWindowControl, hWnd
@@ -178,3 +403,4 @@ start:
 	    .endif
         ret 
     WndProc endp
+    end start
